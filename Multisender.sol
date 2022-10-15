acc676b0f1759f99634f1d24aca8ec77cf53d238
shashireddy42 <85269562+shashireddy42@users.noreply.github.com>
2022-10-15 11:19:28 +0530

    Update Multisender.sol

--- Multisender.sol
+++ Multisender.sol
@@ -2,24 +2,29 @@
 pragma solidity ^0.8.10;
 contract Multisender  {
     //Stores the employess address in array
-    address payable [] public Employees; 
-
-    constructor(address payable [] memory _addrs) {
-        for(uint i=0; i<_addrs.length; i++){
-            Employees.push(_addrs[i]);
+    address[] public Employees; 
+    address payable public owner;
+    uint public share;
+        constructor(){
+         owner = payable(msg.sender);   
         }
+    function AddEmployee(address _addrs) public {
+       Employees.push(_addrs);
     }
     event PaymentReceived(address _from, uint _amount);
 
     //recieve function is to get the ether from SENDER 
     receive() payable external {
+                 
+    }      
+    function Transfer() payable public{
         //transfers the equal ammount of ether for number of employess
-        uint256 share = msg.value / Employees.length; 
-
-        for(uint i=0; i < Employees.length; i++){
-            Employees[i].transfer(share);
+        share = address(this).balance / Employees.length;
+         for(uint i=0; i < Employees.length; i++){
+            (bool sent,)=payable(Employees[i]).call{value:share}("");
+            require(sent,"Transfer Failed");
         }    
         //emits the information about sender and How much he sends to contract
-        emit PaymentReceived(msg.sender, msg.value);
-    }      
+        emit PaymentReceived(msg.sender, address(this).balance);
+    } 
 }
